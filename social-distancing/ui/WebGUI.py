9c89eb4c3401aff3cff2709908ae2a2d03062aa6
Json <alec.sadler.2012@gmail.com>
2020-04-06 07:32:39 +0000

    Manually added WebGUI.py from mrn-dev PR #3

--- social-distancing/ui/WebGUI.py
+++ social-distancing/ui/WebGUI.py
@@ -5,10 +5,17 @@ from flask import Flask
 from flask import render_template
 from flask import Response
 
-from .utils import visualization_util as vis_util
+from .utils import visualization_utils as vis_util
+
+category_index = {
+    "id": 0,
+    "name": "safe",
+    "id": 1,
+    "name": "unsafe",
+}  # TODO: json file for detector config
+
 
 class WebGUI:
-
     def __init__(self, config, engine_instance):
         self.config = config
         self.__ENGINE_INSTANCE = engine_instance
@@ -16,17 +23,34 @@ class WebGUI:
         self._lock = threading.Lock()
         self.app = self.create_flask_app()
 
-    def update(self, input_frame, boxes, distances):
-        # TODO: docstring after completing
-        # TODO: using vis_util for visualizing
+    def update(self, input_frame, nn_out, distances):
+        """
+        Args:
+            input_frame: uint8 numpy array with shape (img_height, img_width, 3)
+            nn_out: a list of dicionary contains normalized numbers of bounding boxes {'id' : '0-0', 'box' : [x, y, h, w], 'score' : 0.99(optional} of shape [N, 3] or [N, 2]
+            distances: a symmetric matrix of normalized distances
+
+        Returns:
+            draw the bounding boxes to an output frame
+        """
+        output_dict = vis_util.visualization_preparation(nn_out, distances)
+        vis_util.visualize_boxes_and_labels_on_image_array(
+            input_frame,
+            output_dict["detection_boxes"],
+            output_dict["detection_classes"],
+            output_dict["detection_scores"],
+            category_index,
+            instance_masks=output_dict.get("detection_masks"),
+            use_normalized_coordinates=True,
+            line_thickness=3,
+        )
         with self._lock:
             self._output_frame = input_frame.copy()
-            self._output_frame = cv.resize(self._output_frame, (220,160))
-
 
     def create_flask_app(self):
         app = Flask(__name__)
-        #@staticmethod
+
+        # @staticmethod
         @app.route("/")
         def _index():
             return render_template("index.html")
@@ -44,7 +68,7 @@ class WebGUI:
     def _generate(self):
         # TODO: docstring after completing
         while True:
-            with self._lock: 
+            with self._lock:
                 # check if the output frame is available, otherwise skip
                 # the iteration of the loop
                 if self._output_frame is None:
@@ -67,7 +91,7 @@ class WebGUI:
         )
 
     def start(self):
-        threading.Thread(target = self._run).start()
+        threading.Thread(target=self._run).start()
         time.sleep(1)
-        video_path = self.config.get_section_dict('App')['VideoPath']
+        video_path = self.config.get_section_dict("App")["VideoPath"]
         self.__ENGINE_INSTANCE.process_video(video_path)
