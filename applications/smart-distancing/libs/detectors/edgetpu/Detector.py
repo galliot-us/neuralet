c1bcb68ed3665596b1cf794e33dfcc721ee7db7d
mrn-mln <mehraliangit@gmail.com>
2020-04-11 01:16:20 +0430

    Documenting Detector module

--- applications/smart-distancing/libs/detectors/edgetpu/Detector.py
+++ applications/smart-distancing/libs/detectors/edgetpu/Detector.py
@@ -1,12 +1,19 @@
-
 class Detector():
+    """
+    Detector class is a high level class for detecting object using edgetpu devices.
+    When an instance of the Detector is created you can call inference method and feed your
+    input image in order to get the detection results.
+
+    :param config: Is a ConfigEngine instance which provides necessary parameters.
+    """
     def __init__(self, config):
         self.config = config
-        self.net = None 
+        self.net = None
+        # Get model name from the config
         self.name = self.config.get_section_dict('Detector')['Name']
-        if self.name == 'mobilenet_ssd_v2' : # or mobilenet_ssd_v1
+        if self.name == 'mobilenet_ssd_v2':  # or mobilenet_ssd_v1
             from . import MobileNetSSD
-            self.net = MobileNetSSD.Detector(self.config) 
+            self.net = MobileNetSSD.Detector(self.config)
         else:
             raise ValueError('Not supported network named: ', self.name)
 
