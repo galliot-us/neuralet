2e02c12e4e92739b2671bf49675172ede7d6432f
Emma Williams <emma.williams.dev@gmail.com>
2020-04-11 16:55:57 -0700

    revised Detectors documetation

--- applications/smart-distancing/libs/detectors/jetson/Detector.py
+++ applications/smart-distancing/libs/detectors/jetson/Detector.py
@@ -11,13 +11,13 @@ class Detector():
         self.net = None
         # Get model name from the config
         self.name = self.config.get_section_dict('Detector')['Name']
-        if self.name == 'ssd_mobilenet_v2_coco':  # or mobilenet_ssd_v1
+        if self.name == 'ssd_mobilenet_v2_coco': 
             from . import MobileNetSSDV2
             self.net = MobileNetSSDV2.Detector(self.config)
         else:
             raise ValueError('Not supported network named: ', self.name)
 
     def inference(self, resized_rgb_image):
-        # Here should inference on the image, output a list of objects, each obj is a dict with two keys "id" and "bbox" 
-        # return [{"id": 0, "bbox": [x, y, w, h]}, {...}, {...}, ...]
+        # Here should inference on the image, output a list of objects, each obj is a dict with two keys "id" and "bbox" and "score" 
+        # return [{"id": 0, "bbox": [x1, y1, x2, y2], "score":s%}, {...}, {...}, ...]
         return self.net.inference(resized_rgb_image)
