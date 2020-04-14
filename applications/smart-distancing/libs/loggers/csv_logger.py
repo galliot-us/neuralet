5be1522d773184e1c8886ef2d30822ea96fde23c
alpha-carinae29 <alpha.carinae29@gmail.com>
2020-04-14 10:15:39 -0700

    correct some typo error @mrn-mln mentioned in his review

--- applications/smart-distancing/libs/loggers/csv_logger.py
+++ applications/smart-distancing/libs/loggers/csv_logger.py
@@ -5,37 +5,34 @@ from datetime import date
 import numpy as np
 
 
-def prepare_object(object_item, frame_number):
-    """construct a dictionary that is appropriate for csv writer.
+def prepare_object(detected_object, frame_number):
+    """Construct a dictionary that is appropriate for csv writer.
 
-    this function transform a dictionary with list values to a dictionary
+    This function transform a dictionary with list values to a dictionary
     with scalar values. This transformation is necessary for csv writer to avoid
     writing lists into csv.
 
     Args:
-        object_item: it is a dictionary that contains an detected object information after postprocessing
+        detected_object: It is a dictionary that contains an detected object information after postprocessing.
         frame_number: current frame number
 
     Returns:
-        A transformed version of object_item to a dictionary with only scalar values. It also contains an item
+        A transformed version of detected_object to a dictionary with only scalar values. It also contains an item
         for frame number.
 
     """
     object_dict = {}
     object_dict.update({"frame_number": frame_number})
-    for key, value in object_item.items():
+    for key, value in detected_object.items():
         if isinstance(value, (list, tuple)):
             for i, item in enumerate(value):
-                if isinstance(item, float):
-                    item = round(item, 4)
-                elif isinstance(item, np.float32):
+                # TODO: Inspect why some items are float and some are np.float32
+                if isinstance(item, (float, np.float32)):
                     item = round(float(item), 4)
-
                 object_dict.update({str(key) + "_" + str(i): item})
         else:
-            if isinstance(value, float):
-                value = round(value, 4)
-            elif isinstance(value, np.float32):
+            # TODO: Inspect why some items are float and some are np.float32
+            if isinstance(value, (float, np.float32)):
                 value = round(float(value), 4)
             object_dict.update({key: value})
     return object_dict
@@ -44,15 +41,14 @@ def prepare_object(object_item, frame_number):
 class Logger:
     """A CSV logger class that store objects information and violated distances information into csv files.
 
-    this logger creates two csv file every day in two different directory, one for logging detected objects
-    and one for logging violated social distancing incidents. the file names are the same as recording date.
+    This logger creates two csv file every day in two different directory, one for logging detected objects
+    and one for logging violated social distancing incidents. The file names are the same as recording date.
 
-    Attributes:
-        config: a ConfigEngine object which store all of the config parameters. access  to any parameter
+        :param config: A ConfigEngine object which store all of the config parameters. Access to any parameter
         is possible by calling get_section_dict method.
-        log_directory: the parent directory that stores all log file.
-        objects_log_directory: a directory inside the log_directory that stores object log files.
-        distances_log_directory: a directory inside the log_directory that stores violated distances log files.
+        :param log_directory: The parent directory that stores all log file.
+        :param objects_log_directory: A directory inside the log_directory that stores object log files.
+        :param distances_log_directory: A directory inside the log_directory that stores violated distances log files.
 
 
     """
@@ -69,33 +65,30 @@ class Logger:
         if not os.path.exists(self.distances_log_directory):
             os.mkdir(self.distances_log_directory)
 
-    def update(self, frame_number, object_list, distances):
-        """write the object and violated distances information of a frame into log files.
+    def update(self, frame_number, objects_list, distances):
+        """Write the object and violated distances information of a frame into log files.
 
-        Args:
-            frame_number: current frame number
-            object_list: a list of dictionary where each dictionary stores information of an object (person) in a frame.
-            distances: a 2-d numpy array that stores distance between each pair of objects.
+        Args: frame_number: current frame number objects_list: A list of dictionary where each dictionary stores
+        information of an object (person) in a frame. distances: A 2-d numpy array that stores distance between each
+        pair of objects.
         """
         file_name = str(date.today())
         objects_log_file_path = os.path.join(self.objects_log_directory, file_name + ".csv")
         distances_log_file_path = os.path.join(self.distances_log_directory, file_name + ".csv")
-        self.log_objects(object_list, frame_number, objects_log_file_path)
+        self.log_objects(objects_list, frame_number, objects_log_file_path)
         self.log_distances(distances, frame_number, distances_log_file_path)
 
     @staticmethod
-    def log_objects(object_list, frame_number, file_path):
-        """write objects information of a frame into the object log file.
-        each row of the object log file consist of a detected object (person) information such as
+    def log_objects(objects_list, frame_number, file_path):
+        """Write objects information of a frame into the object log file.
+        Each row of the object log file consist of a detected object (person) information such as
         object (person) ids, bounding box coordinates and frame number.
 
-        Args:
-            object_list: a list of dictionary where each dictionary stores information of an object (person) in a frame.
-            frame_number: current frame number
-            file_path: log file path
+        Args: objects_list: A list of dictionary where each dictionary stores information of an object (person) in a
+        frame. frame_number: current frame number file_path: log file path
         """
-        if len(object_list) != 0:
-            object_dict = list(map(lambda x: prepare_object(x, frame_number), object_list))
+        if len(objects_list) != 0:
+            object_dict = list(map(lambda x: prepare_object(x, frame_number), objects_list))
 
             if not os.path.exists(file_path):
                 with open(file_path, "w", newline="") as csvfile:
@@ -109,13 +102,13 @@ class Logger:
                 writer.writerows(object_dict)
 
     def log_distances(self, distances, frame_number, file_path):
-        """write violated incident's information of a frame into the object log file
+        """Write violated incident's information of a frame into the object log file.
 
-        each row of the distances log file consist of a violation information such as object (person) ids,
+        Each row of the distances log file consist of a violation information such as object (person) ids,
         distance between these two object and frame number.
 
         Args:
-            distances: a 2-d numpy array that stores distance between each pair of objects.
+            distances: A 2-d numpy array that stores distance between each pair of objects.
             frame_number: current frame number
             file_path: log file path
         """
@@ -134,13 +127,13 @@ class Logger:
                                "distance": distances[indices[0], indices[1]]} for indices in violating_objects])
 
     def extract_violating_objects(self, distances):
-        """extract pair of objects that are closer than the distance threshold.
+        """Extract pair of objects that are closer than the distance threshold.
 
         Args:
-            distances: a 2-d numpy array that stores distance between each pair of objects.
+            distances: A 2-d numpy array that stores distance between each pair of objects.
 
         Returns:
-            A 2-d numpy array where each row is the ids of the objects that violated the social distancing.
+            violating_objects: A 2-d numpy array where each row is the ids of the objects that violated the social distancing.
 
         """
         triu_distances = np.triu(distances) + np.tril(10 * np.ones(distances.shape))
--- applications/smart-distancing/libs/loggers/loggers.py
+++ applications/smart-distancing/libs/loggers/loggers.py
@@ -4,16 +4,15 @@ class Logger:
     this class build a layer based on config specification and call update
     method of it based on logging frequency
 
-    Attributes:
-        config: a ConfigEngine object which store all of the config parameters. access  to any parameter
+        :param config: a ConfigEngine object which store all of the config parameters. access  to any parameter
         is possible by calling get_section_dict method.
-        fps: frame rate of the video file, it should be specified by user in config file.
-        name: logger name, at this time only csv_logger is supported. you can implement your own logger
+        :param fps: frame rate of the video file, it should be specified by user in config file.
+        :param name: logger name, at this time only csv_logger is supported. you can implement your own logger
         by following csv_logger implementation as an example.
-        logger: Logger object which is built based on the name parameter specified in config file.
-        time_interval: specifies how often the logger should log information. For example with time_interval of 0.5
+        :param logger: Logger object which is built based on the name parameter specified in config file.
+        :param time_interval: specifies how often the logger should log information. For example with time_interval of 0.5
         the logger log the information every 0.5 seconds. (set zero to log in every frame)
-        frame_number: keep track of current frame number of the video. This will be used by update method to decide
+        :param frame_number: keep track of current frame number of the video. This will be used by update method to decide
         whether its time to log information or logging should be skipped for this frame (based on logger time interval.)
 
 
@@ -30,18 +29,18 @@ class Logger:
         self.time_interval = float(self.config.get_section_dict("Logger")["TimeInterval"])
         self.frame_number = 0
 
-    def update(self, object_list, distances):
+    def update(self, objects_list, distances):
         """call the update method of the logger.
 
         based on frame_number, fps and time interval, it decides whether to call the
         logger's update method to store the data or not.
 
         Args:
-            object_list: a list of dictionary where each dictionary stores information of an object (person) in a frame.
+            objects_list: a list of dictionary where each dictionary stores information of an object (person) in a frame.
             distances: a 2-d numpy array that stores distance between each pair of objects.
         """
         if self.frame_number % int(self.fps * self.time_interval) == 0:
-            self.logger.update(self.frame_number, object_list, distances)
+            self.logger.update(self.frame_number, objects_list, distances)
             self.frame_number += 1
         else:
             self.frame_number += 1
