59e004d52b8fac4dcfa47ea11b62f27bd837016a
mrn-mln <mehraliangit@gmail.com>
2020-04-21 02:42:16 +0430

    Separating new loggers from old csv_logger file for the sake of future usage

--- applications/smart-distancing/libs/loggers/loggers.py
+++ applications/smart-distancing/libs/loggers/loggers.py
@@ -18,13 +18,20 @@ class Logger:
         # by following csv_logger implementation as an example.
         self.name = self.config.get_section_dict("Logger")["Name"]
         if self.name == "csv_logger":
-            from . import csv_logger
-            self.logger = csv_logger.Logger(self.config)
+            from . import csv_processed_logger
+            self.logger = csv_processed_logger.Logger(self.config)
+
+            # For Logger instance from loggers/csv_logger
+            # region csv_logger
+            # from . import csv_logger
+            # self.logger = csv_logger.Logger(self.config)
+            # end region
 
         # Specifies how often the logger should log information. For example with time_interval of 0.5
         # the logger log the information every 0.5 seconds.
         self.time_interval = float(self.config.get_section_dict("Logger")["TimeInterval"])  # Seconds
         self.submited_time = 0
+        # self.frame_number = 0  # For Logger instance from loggers/csv_logger
 
     def update(self, objects_list, distances):
         """call the update method of the logger.
@@ -36,6 +43,13 @@ class Logger:
             objects_list: a list of dictionary where each dictionary stores information of an object (person) in a frame.
             distances: a 2-d numpy array that stores distance between each pair of objects.
         """
+
         if time.time() - self.submited_time > self.time_interval:
             self.logger.update(objects_list, distances)
             self.submited_time = time.time()
+            # For Logger instance from loggers/csv_logger
+            # region
+            # self.logger.update(self.frame_number, objects_list, distances)
+            # self.frame_number += 1
+            # end region
+
