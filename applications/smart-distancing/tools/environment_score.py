eaa47aa230d952706e93ec502b5071603f6af8a3
mrn-mln <mehraliangit@gmail.com>
2020-04-21 22:03:16 +0430

    add two approache for calculating environment score

--- applications/smart-distancing/tools/environment_score.py
+++ applications/smart-distancing/tools/environment_score.py
@@ -3,9 +3,16 @@ Documentation after implementing
 """
 import numpy as np
 
-MAX_CAPACITY = 10  # The maximum number of people that can stand as far away from other people as possible
+MAX_ACCEPTABLE_CAPACITY = 10  # The maximum number of people that can stand as far away from other people as possible
+MAX_CAPACITY = 20  # The maximum number of people in the environment
 
 
-def max_capasity_environment_scoring(violating_pedestrians: int, ) -> np.float:
-    env_score = 1 - np.minimum((MAX_CAPACITY / violating_pedestrians), 1)
+def mx_environment_scoring_consider_crowd(detected_pedestrians: int, violated_pedestrians: int) -> np.float64:
+    env_score = 1 - np.minimum(
+        ((violated_pedestrians + detected_pedestrians) / (MAX_CAPACITY + MAX_ACCEPTABLE_CAPACITY)), 1)
+    return env_score
+
+
+def mx_environment_scoring(violated_pedestrians: int) -> np.float64:
+    env_score = 1 - np.minimum((violated_pedestrians / MAX_CAPACITY), 1)
     return env_score
