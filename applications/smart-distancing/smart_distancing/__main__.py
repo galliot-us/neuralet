73e03dd89620f3599b5f1f9b524fb9f14047b8fb
Michael de Gans <47511965+mdegans@users.noreply.github.com>
2020-04-30 14:52:44 -0700

    move entrypoint

---
+++ applications/smart-distancing/smart_distancing/__main__.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+import smart_distancing as sd
+
+from typing import (
+    Union,
+)
+
+__all__ = ['main']
+
+def main(config: Union[str, sd.core.ConfigEngine]) -> int:
+    """
+    Main entrypoint for smart_distancing.
+
+    :param config: path to a config file or a ConfigEngine instance.
+    :return: an integer return code for sys.exit()
+    """
+    app = sd.core.DefaultDistancing(config)
+    app.ui.start()
+    return 0
+
+if __name__ == '__main__':
+    import sys
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--config', required=True)
+    args = parser.parse_args()
+
+    # convert the args to a dict and unpack it
+    # allowing easier reuse of main without argparse
+    # it also has the side benefit of enforcing:
+    # argparse arguments == main arguments
+    sys.exit(main(**vars(args)))
