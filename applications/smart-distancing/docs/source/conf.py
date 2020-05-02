6e3c886a0db770c2f0002772e09ccc2502c7f2df
Michael de Gans <47511965+mdegans@users.noreply.github.com>
2020-05-02 13:23:54 -0700

    Google docstring style support

--- applications/smart-distancing/docs/source/conf.py
+++ applications/smart-distancing/docs/source/conf.py
@@ -33,6 +33,7 @@ extensions = [
     'sphinx.ext.todo',
     'sphinx.ext.autodoc',
     'sphinx.ext.doctest',
+    'sphinx.ext.napoleon',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
