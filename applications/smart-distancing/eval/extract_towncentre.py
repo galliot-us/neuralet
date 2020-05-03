d5ae5857959f4800362fef4ad57c5a024fbd39d0
mrn-mln <mehraliangit@gmail.com>
2020-05-04 01:40:33 +0430

    Add docstring

--- applications/smart-distancing/eval/extract_towncentre.py
+++ applications/smart-distancing/eval/extract_towncentre.py
@@ -3,10 +3,19 @@ import cv2
 import numpy as np
 import _init_paths
 
-#Dataset from http://www.robots.ox.ac.uk/ActiveVision/Research/Projects/2009bbenfold_headpose/project.html#datasets
+
+# Dataset from http://www.robots.ox.ac.uk/ActiveVision/Research/Projects/2009bbenfold_headpose/project.html#datasets
 def video2im(src, test_path='../data/test_images', factor=2):
     """
     Extracts all test frames from a video and saves them as jpgs
+
+    Args:
+        src: The image source
+        test_path: The path of images
+        factor: Used for reducing the size of images by factor Ex: img_size = (width/factor, height/factor,3)
+
+    Returns:
+
     """
     if not os.path.exists(test_path):
         os.mkdir(test_path)
@@ -16,20 +25,21 @@ def video2im(src, test_path='../data/test_images', factor=2):
     frame = 0
     cap = cv2.VideoCapture(src)
     length = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
-    print('Total Frame Count:', length )
+    print('Total Frame Count:', length)
 
-    while frame <=4500:
+    while frame <= 4500:
         check, img = cap.read()
         if check:
-            if frame >= 3700 and frame <=4500:
+            if frame >= 3700 and frame <= 4500:
                 img = cv2.resize(img, (1920 // factor, 1080 // factor))
                 cv2.imwrite(os.path.join(test_path, str(frame) + ".jpg"), img)
-                print('Processed: ',frame, end = '\r')
+                print('Processed: ', frame, end='\r')
             frame += 1
         else:
             break
-    print('test set is created successfully at "%s"' % test_path) 
+    print('test set is created successfully at "%s"' % test_path)
     cap.release()
+
 
 if __name__ == '__main__':
     video2im('../data/TownCentreXVID.avi')
