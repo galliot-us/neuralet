4bad0bd6e68feb3f516320b8e41aed991b1f5249
Michael de Gans <47511965+mdegans@users.noreply.github.com>
2020-05-08 13:39:09 -0700

    add tests for network sources

--- applications/smart-distancing/test/test_gst_engine.py
+++ applications/smart-distancing/test/test_gst_engine.py
@@ -1,4 +1,6 @@
 import unittest
+import urllib.request
+import time
 import doctest
 import os
 import sys
@@ -21,8 +23,21 @@ VIDEO_FILENAMES = (
     'TownCentreXVID.avi',
 )
 
-
 from smart_distancing.detectors.deepstream import _gst_engine
+from smart_distancing.detectors.deepstream._gst_engine import GstEngine
+from smart_distancing.detectors.deepstream._ds_config import GstConfig
+
+def freedesktop_is_up():
+    """
+    Test if our freedesktop.org is online and accessable,
+    because it frequently is not. Test shouldn't fail because of
+    potato servers.
+    """
+    try:
+        urllib.request.urlopen('http://www.freedesktop.org/', timeout=2)
+        return True
+    except urllib.request.URLError as err: 
+        return False
 
 class TestGstEngine(unittest.TestCase):
 
@@ -32,6 +47,39 @@ class TestGstEngine(unittest.TestCase):
             doctest.testmod(_gst_engine)[0],
             0,
         )
+
+    def test_source_network_single(self):
+        if freedesktop_is_up():
+            source_configs = [
+                {'uri': 'https://www.freedesktop.org/software/gstreamer-sdk/data/media/sintel_trailer-480p.webm'},
+            ]
+            infer_configs = [dict(),]
+            config = GstConfig(infer_configs, source_configs)
+            engine = GstEngine(config)
+            engine.start()
+            time.sleep(1)
+            engine.stop()
+            engine.join(10)
+            self.assertEqual(engine.exitcode, 0)
+        else:
+            self.skipTest('freedesktop.org is down (as usual)')
+
+    def test_source_network_multiple(self):
+        if freedesktop_is_up():
+            source_configs = [
+                {'uri': 'https://www.freedesktop.org/software/gstreamer-sdk/data/media/sintel_trailer-480p.webm'},
+                {'uri': 'https://www.freedesktop.org/software/gstreamer-sdk/data/media/sintel_trailer-480p.webm'},
+            ]
+            infer_configs = [dict(),]
+            config = GstConfig(infer_configs, source_configs)
+            engine = GstEngine(config)
+            engine.start()
+            time.sleep(1)
+            engine.stop()
+            engine.join(10)
+            self.assertEqual(engine.exitcode, 0)
+        else:
+            self.skipTest('freedesktop.org is down (as usual)')
 
 if __name__ == "__main__":
     import logging
