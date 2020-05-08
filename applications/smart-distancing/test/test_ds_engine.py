4b508812536b3482431ad4c2778db3505ca68624
Michael de Gans <47511965+mdegans@users.noreply.github.com>
2020-05-08 15:52:38 -0700

    reoganize, fix paths

--- applications/smart-distancing/test/test_ds_engine.py
+++ applications/smart-distancing/test/test_ds_engine.py
@@ -15,15 +15,74 @@ THIS_DIR  = os.path.dirname(os.path.abspath(__file__))
 PARENT_DIR = os.path.dirname(THIS_DIR)
 sys.path.insert(0, PARENT_DIR)
 
+from smart_distancing.detectors.deepstream import _ds_engine
+from smart_distancing.detectors.deepstream import _ds_config
+
 # where to find test data
 TEST_DATA_DIR = os.path.join(THIS_DIR, 'data')
 VIDEO_FILENAMES = (
     'TownCentreXVID.avi',
 )
+#deepstream paths
+SAMPLES_PATH = '/opt/nvidia/deepstream/deepstream/samples/'
+MODEL_PATH =  os.path.join(SAMPLES_PATH, 'models')
+CONFIGS_PATH = os.path.join(SAMPLES_PATH, 'configs')
 
+# config generator for uridecodebin
+FILE_SOURCE_CONFIGS = [
+    {'uri': f'file://{fn}'} for fn in VIDEO_FILENAMES
+]  # type: List[Dict[str, str]]
+# a list of paths to sample models
 
-from smart_distancing.detectors.deepstream import _ds_engine
-from smart_distancing.detectors.deepstream import _ds_config
+# Configs for nvinfer elements in primary mode:
+DETECTOR_CONFIGS = [
+    {
+        'model-file': os.path.join(MODEL_PATH, 'Primary_Detector/resnet10.caffemodel'),
+        'proto-file': os.path.join(MODEL_PATH, 'Primary_Detector/resnet10.prototxt'),
+        'labelfile-path': os.path.join(MODEL_PATH, 'Primary_Detector/labels.txt'),
+        'int8-calib-file': os.path.join(MODEL_PATH, 'Primary_Detector/cal_trt.bin'),
+    },
+    {
+        'model-file': os.path.join(MODEL_PATH, 'Primary_Detector_Nano/resnet10.caffemodel'),
+        'proto-file': os.path.join(MODEL_PATH, 'Primary_Detector_Nano/resnet10.prototxt'),
+        'labelfile-path': os.path.join(MODEL_PATH, 'Primary_Detector_Nano/labels.txt'),
+    }
+]  # type: List[Dict[str, str]]
+
+CLASSIFIER_MODEL_DIRS = (
+    'Secondary_CarColor',
+    'Secondary_CarMake',
+    'Secondary_VehicleTypes',
+)
+CLASSIFIER_MODELS = [
+    [
+        os.path.join(d, 'resnet18.caffemodel'),
+        os.path.join(d, 'resnet18.prototxt'),
+        os.path.join(d, 'labels.txt'),
+        os.path.join(d, 'cal_trt.bin'),
+        os.path.join(d, 'mean.ppm'),
+    ] for d in CLASSIFIER_MODEL_DIRS
+]
+# we test every primary model with every secondary model
+# with async mode both on and off
+CLASSIFIER_CONFIGS = []
+for detector_config in DETECTOR_CONFIGS:
+    for async_mode in range(1):
+        for c, p, l, i, m in CLASSIFIER_MODELS:
+            conf = [
+                detector_config,  # the primary config
+                {
+                    # oh fuck me, this can't be set dynamically.
+                    # why is Nvidia in love with config files with craptastic absolute paths.
+                    'model-file': m,
+                    'proto-file': p,
+                    'lablefile-path': l,
+                    'int8-calib-file': i,
+                    'mean-file': m, # bad file. very unfriendly (groans)
+                    'classifier-async-mode': async_mode,
+                },
+            ]
+            CLASSIFIER_CONFIGS.append(conf)
 
 class TestDsEngine(unittest.TestCase):
 
@@ -37,78 +96,13 @@ class TestDsEngine(unittest.TestCase):
             0,
         )
 
-    # config generator for uridecodebin
-    FILE_SOURCE_CONFIGS = [
-        {'uri': f'file://{fn}'} for fn in VIDEO_FILENAMES
-    ]  # type: List[Dict[str, str]]
-    # a list of paths to sample models
-    SAMPLES_PATH = '/opt/nvidia/deepstream/deepstream/samples/models'
-    MODEL_PATH =  os.path.join(SAMPLES_PATH, 'models')
-    CONFIGS_PATH = os.path.join(SAMPLES_PATH, 'configs')
-    DETECTOR_MODELS = (
-        (
-            'PrimaryDetector/resnet10.caffemodel',
-            'PrimaryDetector/resnet10.prototxt',
-            'PrimaryDetector/labels.txt',
-            'PrimaryDetector/cal_trt.bin',  # int8 calibration data for supporting platforms
-        ),
-        (
-            'PrimaryDetector_Nano/resnet10.caffemodel',
-            'PrimaryDetector_Nano/resnet10.prototxt',
-            'PrimaryDetector_Nano/labels.txt',
-            None,  # int8 calibration data for supporting platforms
-        ),
-    )  # type: Tuple[str, str, str, Optional[str]]
-    DETECTOR_CONFIGS = [
-        {
-            'model-file': m,
-            'proto-file': p,
-            'labelfile-path': l,
-            'int8-calib-file': i
-        } for m, p, l, i in DETECTOR_MODELS
-    ]  # type: List[Dict[str, str]]
-    CLASSIFIER_MODEL_DIRS = (
-        'Secondary_CarColor',
-        'Secondary_CarMake',
-        'Secondary_VehicleTypes',
-    )
-    CLASSIFIER_MODELS = [
-        [
-            os.path.join(d, 'resnet18.caffemodel'),
-            os.path.join(d, 'resnet18.prototxt'),
-            os.path.join(d, 'labels.txt'),
-            os.path.join(d, 'cal_trt.bin'),
-            os.path.join(d, 'mean.ppm'),
-        ] for d in CLASSIFIER_MODEL_DIRS
-    ]
-    # we test every primary model with every secondary model
-    # with async mode both on and off
-    CLASSIFIER_CONFIGS = []
-    for detector_config in DETECTOR_CONFIGS:
-        for async_mode in range(1):
-            for c, p, l, i, m in CLASSIFIER_MODELS:
-                conf = [
-                    detector_config,  # the primary config
-                    {
-                        # oh fuck me, this can't be set dynamically.
-                        # why is Nvidia in love with config files with craptastic absolute paths.
-                        'model-file': m,
-                        'proto-file': p,
-                        'lablefile-path': l,
-                        'int8-calib-file': i,
-                        'mean-file': m, # bad file. very unfriendly (groans)
-                        'classifier-async-mode': async_mode,
-                    },
-                ]
-                CLASSIFIER_CONFIGS.append(conf)
-
     #FIXME(mdegans): right now this fails because some properties can't be set on nvinfer
     # without using an .ini file, so modifying DsEngine will be necessary.
     def test_start_stop(self):
         """test pipeline construct/destruct"""
         # this is copypasta from the TestGstEngine doctest
-        single_detector_configs = [self.DETECTOR_CONFIGS[0],]
-        single_file_configs = [self.FILE_SOURCE_CONFIGS[0],]
+        single_detector_configs = [DETECTOR_CONFIGS[0],]
+        single_file_configs = [FILE_SOURCE_CONFIGS[0],]
 
         config = _ds_config.DsConfig(single_detector_configs, single_file_configs)
         engine = _ds_engine.DsEngine(config)
