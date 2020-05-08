97e1196127754f2e3d9fbd83e16dc2e3712228b4
Michael de Gans <47511965+mdegans@users.noreply.github.com>
2020-05-08 13:58:30 -0700

    more logging it became useful again

--- applications/smart-distancing/test/test_gst_engine.py
+++ applications/smart-distancing/test/test_gst_engine.py
@@ -106,5 +106,5 @@ class TestGstEngine(unittest.TestCase):
 
 if __name__ == "__main__":
     import logging
-    logging.basicConfig(level=logging.INFO)
+    logging.basicConfig(level=logging.DEBUG)
     unittest.main(verbosity=2)
