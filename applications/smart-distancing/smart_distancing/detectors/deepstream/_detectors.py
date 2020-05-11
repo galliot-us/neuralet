af310725051dc13ce59b4f51c7117928aabddfbe
Michael de Gans <47511965+mdegans@users.noreply.github.com>
2020-05-11 15:05:17 -0700

    WIP initial stub for the detector this almost assuredly  doesn't work

--- applications/smart-distancing/smart_distancing/detectors/deepstream/_detectors.py
+++ applications/smart-distancing/smart_distancing/detectors/deepstream/_detectors.py
@@ -1,8 +1,33 @@
 """DsDetector goes here"""
 
-from smart_distancing.core import BaseDistancing
+from smart_distancing.core import ConfigEngine
+from smart_distancing.detectors import BaseDetector
+from smart_distancing.detectors.deepstream import DsEngine
+from smart_distancing.detectors.deepstream import DsConfig
+
+from typing import (
+    Dict,
+    Tuple,
+)
 
 __all__ = ['DsDetector']
 
-class DsDetector(BaseDistancing):
-    """DeepStream implementation of BaseDistancing"""
+class DsDetector(BaseDetector):
+    """
+    DeepStream implementation of BaseDetector.
+    """
+
+    engine = None  # type: DsEngine
+
+    def load_model(self):
+        ds_config = (self.config)
+        self.engine = DsEngine(ds_config)
+        self.engine.start()
+
+    def inference(self):
+        self.on_frame(self.engine.results)
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
