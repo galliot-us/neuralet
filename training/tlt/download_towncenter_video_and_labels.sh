1197189c3986faea2a71fc42a77b0504bb09ef42
alpha-carinae29 <alpha.carinae29@gmail.com>
2020-05-19 01:53:42 -0700

    add download video and label script

---
+++ training/tlt/download_towncenter_video_and_labels.sh
@@ -0,0 +1,3 @@
+mkdir "/experiment_dir/dataset"
+wget http://www.robots.ox.ac.uk/ActiveVision/Research/Projects/2009bbenfold_headpose/Datasets/TownCentreXVID.avi -O experiment_dir/dataset/TownCentreXVID.avi
+wget https://www.robots.ox.ac.uk/ActiveVision/Research/Projects/2009bbenfold_headpose/Datasets/TownCentre-groundtruth.top -O experiment_dir/dataset/TownCentre-groundtruth.top
