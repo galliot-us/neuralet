39a2f336a6e57b2ee4ae724435f15ff6876ad926
alpha-carinae29 <alpha.carinae29@gmail.com>
2020-05-19 21:38:48 +0000

    complete the notebook and add docstring

--- training/tlt/extract_images.py
+++ training/tlt/extract_images.py
@@ -5,6 +5,11 @@ import cv2 as cv
 
 
 def extract_images_from_video(args):
+    """
+    This function extract each frame from the Oxford Town Center video file, resize it to a desired size and save them
+    in the `images` directory in `jpg` format.
+    User should specify the video path and the width and height of the desired output images.
+    """
     video_path = Path(args.video_path)
     img_width = args.image_width
     img_height = args.image_height
@@ -15,10 +20,10 @@ def extract_images_from_video(args):
     frame = 0
     video_cap = cv.VideoCapture(str(video_path))
     print("==================== Start Reading Video ! ====================")
-    while frame <= 4500:
+    while frame <= 4500:  # only first 4501 frames of Oxford Town Center Dataset has annotations.
         ret, img = video_cap.read()
         img = cv.resize(img, (img_width, img_height))
-        image_path = os.path.join(images_dir, str(frame)+".jpg")
+        image_path = os.path.join(images_dir, str(frame) + ".jpg")
         cv.imwrite(image_path, img)
         frame += 1
         print("frame number {0} extracted".format(frame), end="\r")
