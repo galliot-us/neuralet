bcde98ab01ca585f581b3a86402a444b156f7b89
mrn-mln <mehraliangit@gmail.com>
2020-05-23 04:35:10 +0430

    fixed miner issues

--- applications/smart-distancing/eval/utils/exporter.py
+++ applications/smart-distancing/eval/utils/exporter.py
@@ -2,25 +2,26 @@ import numpy as np
 import os
 
 
-def export_results(nn_out, class_name, path, image_name):
-    w = 300
-    h = 300
+def export_results(nn_out, class_name, path, image_name, cv_img):
+    img_resolution = cv_img.shape
+    img_h, img_w = img_resolution[0], img_resolution[1]
     results = ''
     dt_fromat = "{0} {1} {2} {3} {4} {5} \n"
     for obj in nn_out:
         bbox = obj['bbox']
         bbox[0] = np.maximum(0, bbox[0])
         bbox[1] = np.maximum(0, bbox[1])
-        bbox[2] = np.minimum(w, bbox[2])
-        bbox[3] = np.minimum(h, bbox[3])
-        x0 = bbox[0]
-        y0 = bbox[1]
-        x1 = bbox[2]
-        y1 = bbox[3]
+        bbox[2] = np.minimum(1, bbox[2])
+        bbox[3] = np.minimum(1, bbox[3])
+        x0 = bbox[1] * img_w
+        y0 = bbox[0] * img_h
+        x1 = bbox[3] * img_w
+        y1 = bbox[2] * img_h
         width = x1 - x0
         height = y1 - y0
         score = obj['score']
-        results += dt_fromat.format(str(class_name), str(score), str(x0), str(y0), str(width),
+        cls = class_name[obj["id"]]
+        results += dt_fromat.format(str(cls), str(score), str(x0), str(y0), str(width),
                                     str(height))
 
         out_file = os.path.join(path, image_name + '.txt')
