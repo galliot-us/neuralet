d581bb49ce67746bccb9cfb46360f0894335904b
alpha-carinae29 <alpha.carinae29@gmail.com>
2020-06-17 14:16:10 -0700

    add config file and its parameters in code

--- applications/smart-distancing/bootstrap/teachers/model_builder.py
+++ applications/smart-distancing/bootstrap/teachers/model_builder.py
@@ -4,6 +4,6 @@ TEACHER_MAP = {"iterdet": IterDet}
 
 
 def build(config):
-    model_name = config.get_section_dict('Detector')['Name']
+    model_name = config.get_section_dict('Teacher')['Name']
     model = TEACHER_MAP[model_name]
     return model(config=config)
