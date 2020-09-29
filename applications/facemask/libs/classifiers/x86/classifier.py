7ce87df07527247b01fd0d6a544111e3ce68e77d
mrn-mln <mehraliangit@gmail.com>
2020-09-29 23:57:19 +0330

    added classifiers parameters

---
+++ applications/facemask/libs/classifiers/x86/classifier.py
@@ -0,0 +1,22 @@
+
+class Classifier:
+    """
+    Classifier class is a high level class for classifying images using x86 devices.
+    When an instance of the Classifier is created you can call inference method and feed your
+    input image in order to get the classifier results.
+    :param config: Is a ConfigEngine instance which provides necessary parameters.
+    """
+    def __init__(self, config):
+        self.config = config
+        self.name = self.config.CLASSIFIER_NAME
+
+        if self.name == 'face_mask':
+            from libs.classifiers.x86 import face_mask
+            self.net = face_mask.Classifier(self.config)
+        else:
+            raise ValueError('Not supported network named: ', self.name)
+
+    def inference(self, resized_rgb_image):
+        self.fps = self.net.fps
+        output = self.net.inference(resized_rgb_image)
+        return output
