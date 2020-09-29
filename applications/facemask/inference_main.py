78347d16d38276f9c1c260f62c17e5c7e9d9f90c
mrn-mln <mehraliangit@gmail.com>
2020-09-29 23:31:47 +0000

    updated

--- applications/facemask/inference_main.py
+++ applications/facemask/inference_main.py
@@ -1,14 +1,21 @@
 from configs.config_handler import Config
 from libs.classifiers.x86.classifier import Classifier
+from models.frontend import FacemaskClassifierModel
+
 
 def main():
     config_path = 'configs/config.json'
     print("-_- -_- -_- -_- -_- -_- -_- Running %s -_- -_- -_- -_- -_- -_- -_-" % config_path)
     cfg = Config(path=config_path)
-    cls_model = Classifier(cfg)
+    classifier = FacemaskClassifierModel(cfg)
+    model = classifier.model
+    
+    cls_model = Classifier(model, cfg)
     import numpy as np
 
-    img = np.random.random(size=[1,45, 45, 3])
+    img = np.random.random(size=[2,45, 45, 3])
     result = cls_model.inference(img)
-    print(img)
+    print(result)
 
+if __name__ == "__main__":
+    main()
