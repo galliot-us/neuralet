3ce9313f6d8fa0639931a9cadeca9c5d8aef5f51
mrn-mln <mehraliangit@gmail.com>
2020-09-30 03:46:23 +0330

    mock inference from detector

--- applications/facemask/inference_main.py
+++ applications/facemask/inference_main.py
@@ -1,5 +1,6 @@
 from configs.config_handler import Config
 from libs.classifiers.x86.classifier import Classifier
+from libs.detectors.x86.detector import Detector
 from models.frontend import FacemaskClassifierModel
 
 
@@ -9,13 +10,20 @@ def main():
     cfg = Config(path=config_path)
     classifier = FacemaskClassifierModel(cfg)
     model = classifier.model
-    
+
     cls_model = Classifier(model, cfg)
     import numpy as np
 
-    img = np.random.random(size=[2,45, 45, 3])
+    # resized_image = cv.resize(cv_image, tuple(self.image_size[:2]))
+    # rgb_resized_image = cv.cvtColor(resized_image, cv.COLOR_BGR2RGB)
+    rgb_resized_image = np.random.random(size=[cfg.DETECTOR_INPUT_SIZE, cfg.DETECTOR_INPUT_SIZE, 3])
+    object_list = Detector.inference(rgb_resized_image)
+    print(object_list)
+
+    img = np.random.random(size=[2, 45, 45, 3])
     result = cls_model.inference(img)
     print(result)
+
 
 if __name__ == "__main__":
     main()
