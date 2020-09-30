998dae7484cc4b52f2c309139fc31153703b4f3e
mrn-mln <mehraliangit@gmail.com>
2020-09-30 03:37:03 +0330

    added detector module

---
+++ applications/facemask/libs/detectors/x86/detector.py
@@ -0,0 +1,22 @@
+class Detector:
+    """
+    Detector class is a high level class for detecting object using x86 devices.
+    When an instance of the Detector is created you can call inference method and feed your
+    input image in order to get the detection results.
+    :param config: Is a ConfigEngine instance which provides necessary parameters.
+    """
+
+    def __init__(self, config):
+        self.config = config
+        self.name = self.config.DETECTOR_NAME
+
+        if self.name == "openpifpaf":
+            from libs.detectors.x86 import openpifpaf
+            self.net = openpifpaf.Detector(self.config)
+        else:
+            raise ValueError('Not supported network named: ', self.name)
+
+    def inference(self, resized_rgb_image):
+        self.fps = self.net.fps
+        output = self.net.inference(resized_rgb_image)
+        return output
