9e9f8e22e9c56d2481c9fb6f7c6977fc6251d4b4
mrn-mln <mehraliangit@gmail.com>
2020-10-01 14:29:25 +0330

    change license file

--- applications/facemask/inference_main.py
+++ applications/facemask/inference_main.py
@@ -1,18 +1,21 @@
 from configs.config_handler import Config
-from libs.classifiers.x86.classifier import Classifier
-from libs.detectors.x86.detector import Detector
-from models.frontend import FacemaskClassifierModel
 from libs.core import FaceMaskAppEngine as CvEngine
 from ui.web_gui import WebGUI as UI
+from argparse import ArgumentParser
 
 
 def main():
-    config_path = 'configs/config.json'
+    """
+    Creates config and application engine module and starts ui
+    :return:
+    """
+    argparse = ArgumentParser()
+    argparse.add_argument('--config', type=str, help='json config file path', default='configs/config.json')
+    args = argparse.parse_args()
+    config_path = args.config
     print("-_- -_- -_- -_- -_- -_- -_- Running %s -_- -_- -_- -_- -_- -_- -_-" % config_path)
     cfg = Config(path=config_path)
-    classifier = FacemaskClassifierModel(cfg)
-    model = classifier.model
-    
+
     engine = CvEngine(cfg)
     ui = UI(cfg, engine)
     engine.set_ui(ui)
