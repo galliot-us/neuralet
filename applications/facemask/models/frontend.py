caf1e312b56f5b2c3a0b525f26c6d8b79fdbef6a
mrn-mln <mehraliangit@gmail.com>
2020-10-01 14:08:34 +0330

    add docstring to ftontend.py

--- applications/facemask/models/frontend.py
+++ applications/facemask/models/frontend.py
@@ -11,15 +11,44 @@ from tensorflow.keras import regularizers
 from tensorflow.keras.models import load_model
 import tensorflow.compat.v1 as tf
 
+model_backend_dic = {
+    "MobileNet": MobileNetFeature,
+    "OFMClassifier": OFMClassifier
+
+}
+
+def backend_loader(cfg):
+    '''
+    Load backend network based on the backend name at config
+
+    Args:
+        cfg: Is a Config instance which provides necessary parameters.
+    Returns:
+        backend: The backend network which is implemented at backend.py
+    '''
+
+    model_name = cfg.MODEL_NAME.split("-")[0]
+    if model_name in model_backend_dic:
+        backend = model_backend_dic.get(model_name)
+    else:
+        raise ValueError(f"{cfg.MODEL_NAME} is not in defined models")
+    return backend
+
 
 class FacemaskClassifierModel:
+    """
+    Perform image classification with the given model backend name.
+    :param cfg: Is a Config instance which provides necessary parameters.
+    """
     def __init__(self, cfg):
         self._cfg = cfg
         self.input_size = cfg.INPUT_SIZE
         self.channels = cfg.NO_CHANNEL
         input_image = Input(shape=(self.input_size, self.input_size, self.channels))
 
-        nn = OFMClassifier(cfg)
+        # load backend network
+        backend_network = backend_loader(self._cfg)
+        nn = backend_loader(self._cfg)
         features = nn.model(input_image)
         out = Flatten()(features)
         out = Dense(10, activation='relu', name='fc_1')(out)
