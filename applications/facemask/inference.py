352135a28765ece07b7b375e458461642a35c6e3
mrn-mln <mehraliangit@gmail.com>
2020-10-02 23:17:30 +0000

    modified config and inference

--- applications/facemask/inference.py
+++ applications/facemask/inference.py
@@ -1,7 +1,3 @@
-from libs.detectors.x86.detecor import Detector
-from libs.detectors.edgetpu.detector import Detector
-from libs.classifiers.x86.classifier import Classifier
-from libs.classifiers.edgetpu.classifier import Classifier
 from configs.config_handler import Config
 import cv2 as cv
 import PIL
@@ -10,7 +6,7 @@ import numpy as np
 
 def main():
     config_path = 'configs/config.json'
-    input_path = ''
+    input_path = 'data/video/sample2.mov'
     output_path = ''
 
     cfg = Config(path=config_path)
