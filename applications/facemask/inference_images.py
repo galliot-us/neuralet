ddb0c74003020c14ae5a413b6bc22a80b0c6f4cf
mrn-mln <mehraliangit@gmail.com>
2020-11-15 17:07:51 +0330

    modified inference image

--- applications/facemask/inference_images.py
+++ applications/facemask/inference_images.py
@@ -4,6 +4,7 @@ import numpy as np
 from argparse import ArgumentParser
 import os
 
+
 def main():
     """
     Read input images and process them, the output images will be exported output_dir
@@ -14,19 +15,19 @@ def main():
     argparse = ArgumentParser()
     argparse.add_argument('--config', type=str, help='json config file path')
     argparse.add_argument('--input_image_dir', type=str, help='the directory of input images')
-    argparse.add_argument('--output_image_dir', type=str, help='the directory of output images', default='output_images')
+    argparse.add_argument('--output_image_dir', type=str, help='the directory of output images',
+                          default='output_images')
     args = argparse.parse_args()
 
     config_path = args.config
     cfg = Config(path=config_path)
-
 
     input_dir = args.input_image_dir
     print("INFO: The directory of input images is: ", input_dir)
     output_dir = args.output_image_dir
     if not os.path.exists(output_dir):
         os.mkdir(output_dir)
-        
+
     print("INFO: The output images will be exported at: ", output_dir)
 
     detector_input_size = (cfg.DETECTOR_INPUT_SIZE[0], cfg.DETECTOR_INPUT_SIZE[1], 3)
@@ -54,7 +55,7 @@ def main():
         image_path = os.path.join(input_dir, filename)
         raw_img = cv.imread(image_path)
         if np.shape(raw_img) != ():
-            width, height, _ = raw_img.shape
+            height, width, _ = raw_img.shape
             resized_image = cv.resize(raw_img, tuple(detector_input_size[:2]))
             rgb_resized_image = cv.cvtColor(resized_image, cv.COLOR_BGR2RGB)
             objects_list = detector.inference(rgb_resized_image)
@@ -77,7 +78,7 @@ def main():
 
             faces = np.array(faces)
             if np.shape(faces)[0] == 0:
-                print("can not find face at ". image_path)
+                print("can not find face at ".image_path)
                 continue
 
             face_mask_results, scores = classifier_model.inference(faces)
@@ -90,12 +91,13 @@ def main():
                     color = (0, 0, 0)
 
                 cv.rectangle(raw_img, (cor[0], cor[1]), (cor[2], cor[3]), color, 2)
-                cv.imwrite(os.path.join(output_dir, filename), raw_img)
+            cv.imwrite(os.path.join(output_dir, filename), raw_img)
             print('image "{}" are processed. {} fps'.format(image_path, detector.fps))
         else:
             continue
 
     print('INFO: Finish:) Output images are exported at: ', output_dir)
+
 
 if __name__ == '__main__':
     main()
