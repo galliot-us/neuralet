96d497f6925926e643d784625a81953155f3dcb6
alpha-carinae29 <alpha.carinae29@gmail.com>
2020-11-24 16:20:07 -0800

    change model_builder logic to be compatible with different teacher environments

--- applications/adaptive-learning/teachers/model_builder.py
+++ applications/adaptive-learning/teachers/model_builder.py
@@ -1,18 +1,17 @@
-from iterdet import IterDet
-from faster_rcnn_nas import FasterRcnnNas
 import logging
-TEACHER_MAP = {
-    "iterdet": IterDet,
-    "faster_rcnn_nas": FasterRcnnNas
-               }
 
 
 def build(config):
     """ build a teacher model based on config file"""
     model_name = config.get_section_dict('Teacher')['Name']
-    if model_name in TEACHER_MAP.keys():
-        model = TEACHER_MAP[model_name]
+    if model_name == "iterdet":
+        from iterdet import IterDet
+        model = IterDet(config=config)
+        logging.info("model loaded successfully")
+    elif model_name == "faster_rcnn_nas":
+        from faster_rcnn_nas import FasterRcnnNas
+        model = FasterRcnnNas(config=config)
         logging.info("model loaded successfully")
     else:
         raise ValueError("The Teacher model name should be iterdet but {} provided".format(model_name))
-    return model(config=config)
+    return model
