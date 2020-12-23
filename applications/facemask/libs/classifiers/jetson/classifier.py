3ceaa4a78676f428299454a03a3eabd7dec7566f
Undefined References <undefined.references@gmail.com>
2020-12-23 14:56:21 +0330

    face_mask classifier added

--- applications/facemask/libs/classifiers/jetson/classifier.py
+++ applications/facemask/libs/classifiers/jetson/classifier.py
@@ -1,18 +1,21 @@
+import logging
+logging.getLogger().setLevel(logging.INFO)
 
 class Classifier:
     """
-    Classifier class is a high-level class for classifying images using Jetson devices.
+    Classifier class is a high level class for classifying images using x86 devices.
     When an instance of the Classifier is created you can call inference method and feed your
     input image in order to get the classifier results.
-    :param config: Is a Config instance which provides necessary parameters.
+
+    :param config: Is a ConfigEngine instance which provides necessary parameters.
     """
     def __init__(self, config):
         self.config = config
         self.name = self.config.CLASSIFIER_NAME
 
         if self.name == 'OFMClassifier':
-            from libs.classifiers.jetson import face_mask_jetson
-            self.net = face_mask_jetson.Classifier(self.config)
+            from libs.classifiers.jetson import face_mask
+            self.net = face_mask.Classifier(self.config)
         else:
             raise ValueError('Not supported network named: ', self.name)
 
