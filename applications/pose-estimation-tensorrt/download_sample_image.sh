fd1d3a0a12e370792875ce44e19db3765db8d6f1
Mohsen Hejrati <mhejrati@gmail.com>
2021-01-14 22:20:36 -0500

    remove pose estimation, move it to a neuralet/pose-estimation

--- applications/pose-estimation-tensorrt/download_sample_image.sh
+++
@@ -1,1 +0,0 @@
-wget https://raw.githubusercontent.com/vita-epfl/openpifpaf/master/docs/coco/000000081988.jpg -O outputs/000000081988.jpg
--- applications/pose-estimation-tensorrt/download_sample_video.sh
+++
@@ -1,1 +0,0 @@
-wget https://social-distancing-data.s3.amazonaws.com/softbio_vid.mp4 -O outputs/softbio_vid.mp4
